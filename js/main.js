5eb6d7e85cc014a6f07cc55f1bf4a5847c880bf8
RomaVintonyak <19roman23@gmail.com>
2019-12-11 13:51:54 +0200

    create scroll to top button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
-$(function (){
-    /*togler class show to burger button */
-    $("#toglerButton").on("click", function(event) {
-        event.preventDefault();
-        $(".burger__icon").toggleClass("show");
-      });
+$(function() {
+  /*togler class show to burger button */
+  $("#toglerButton").on("click", function(event) {
+    event.preventDefault();
+    $(".burger__icon").toggleClass("show");
+  });
+  /*back to top*/
+  $(function() {
+    $(".back__to--top").hide();
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 200) {
+        $(".back__to--top").fadeIn();
+      } else {
+        $(".back__to--top").fadeOut();
+      }
+    });
+    $(".back__to--top").click(function(event) {
+      event.preventDefault();
+      $("body, html").animate(
+        {
+          scrollTop: 0
+        },
+        800
+      );
+      return false;
+    });
+  });
+  
 });
