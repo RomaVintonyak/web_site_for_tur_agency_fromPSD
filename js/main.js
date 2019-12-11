f698cf2a32e1f2637d995c8216f7d927d1ecca7b
RomaVintonyak <19roman23@gmail.com>
2019-12-11 14:47:23 +0200

    create smoth scroll & fixed header

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function() {
-  /*togler class show to burger button */
+  /*togler class click to burger button */
   $("#toglerButton").on("click", function(event) {
     event.preventDefault();
-    $(".burger__icon").toggleClass("show");
+    $(".burger__icon").toggleClass("click");
   });
   /*back to top*/
   $(function() {
@@ -25,5 +25,40 @@ $(function() {
       return false;
     });
   });
-  
+  /*smoth scroll to section*/
+  $("[data-scroll]").on("click", function(event) {
+    event.preventDefault();
+    var blockId = $(this).data("scroll");
+    var blockOffset = $(blockId).offset().top;
+    /*nav.removeClass("show"); */
+    $("html, body").animate(
+      {
+        scrollTop: blockOffset - 70
+      },
+      800
+    );
+  });
+  /*fixed heade*/
+  var introH = $("#intro").innerHeight(),
+    header = $(".header"),
+    scrollOffset = $(window).scrollTop();
+  checkScroll(scrollOffset);
+  /*fixed header*/
+  $(window).on("scroll", function() {
+    scrollOffset = $(this).scrollTop();
+    checkScroll(scrollOffset);
+  });
+  function checkScroll(scrollOffset) {
+    scrollOffset = $(this).scrollTop();
+    if (scrollOffset >= introH) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  }
+  $(".nav__link").on("click", function(event) {
+    event.preventDefault();
+    $(this).toggleClass("active");
+    $(".nav__link a").toggleClass("active");
+  });
 });
